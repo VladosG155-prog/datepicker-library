58b89fa1cc97c55ee1f03db406fb3704fdbe611e
Vlad <55330664+VladosG155-prog@users.noreply.github.com>
2024-02-23 15:19:49 +0300

    Update modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,32 +1,38 @@
-import { FC, FormEvent, useRef, useState } from 'react'
+import { FC, FormEvent, useEffect, useRef, useState } from 'react'
 import { createPortal } from 'react-dom'
-import { IModalProps } from './Modal.interfaces'
+import { IModalProps } from './interfaces'
 import { useClickOutside } from '../../hooks/useClickOutside'
 
-export const Modal: FC<IModalProps> = ({
-    isOpen,
-    onClose,
-    onSubmit,
-    todos,
-    onRemove,
-}) => {
+export const Modal: FC<IModalProps> = ({ isOpen, onClose, onSubmit, todos, onRemove }) => {
     const [todoText, setTodoText] = useState('')
-    const ref = useRef(null)
-    const handleSubmit = (event: FormEvent) => {
+    const modalRef = useRef(null)
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    useEffect(() => {
+        if (inputRef.current) {
+            inputRef.current.focus()
+        }
+    }, [])
+
+    const handleSubmit = (event: FormEvent): void => {
         event.preventDefault()
         onSubmit(todoText)
         setTodoText('')
         onClose()
-    }   
+    }
 
-
-    useClickOutside(ref, onClose)
+    useClickOutside(modalRef, onClose)
 
     if (!isOpen) return null
 
     return createPortal(
-        <div ref={ref} className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50">
+        <div
+            data-id="modal"
+            className="fixed top-0 left-0 w-full h-full flex justify-center
+            items-center bg-black bg-opacity-50"
+        >
             <form
+                ref={modalRef}
                 onSubmit={handleSubmit}
                 className="bg-white p-8 rounded shadow-lg relative"
             >
@@ -36,10 +42,7 @@ export const Modal: FC<IModalProps> = ({
                         <li key={id} className="flex items-center mb-2">
                             <span className="mr-2">№: {index + 1}</span>
                             <span>{title}</span>
-                            <button
-                                onClick={() => onRemove(id)}
-                                className="ml-2 text-red-500"
-                            >
+                            <button onClick={() => onRemove(id)} className="ml-2 text-red-500">
                                 &#10006;
                             </button>
                         </li>
@@ -48,6 +51,7 @@ export const Modal: FC<IModalProps> = ({
                 <div className="sticky top-1">
                     <input
                         type="text"
+                        ref={inputRef}
                         value={todoText}
                         onChange={(e) => setTodoText(e.target.value)}
                         placeholder="Enter todo..."
@@ -56,7 +60,8 @@ export const Modal: FC<IModalProps> = ({
                     <div className="flex justify-end">
                         <button
                             onClick={onClose}
-                            className="mr-2 bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded"
+                            className="mr-2 bg-gray-300 hover:bg-gray-400
+                            text-gray-800 font-semibold py-2 px-4 rounded"
                         >
                             Cancel
                         </button>
